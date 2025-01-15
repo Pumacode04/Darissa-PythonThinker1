c274fc1ad81f9ceaf7f755a48f32ed160a2caad3
Pumacode04 <vivienht@hotmail.com>
2025-01-15 08:29:56 +0000

    Jan 15, 2025, 8:29 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -10,8 +10,8 @@ print("Hello from lesson 2")
 # check each item's material and place it in the correct bin.
 
 # check if item type is "glass", "plastic", or "paper"
-# repeat until number of items is equal to 
-# if item type is "glass"
+# repeat until number of items is equal to 0
+#if item type is "glass"
 #   place it in the blue bin
 # otherwise if its "plastic"
 #   place it in the green bin
