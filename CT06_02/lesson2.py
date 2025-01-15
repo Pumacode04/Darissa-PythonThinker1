c40a133f09c3706ad03b467e58628341825a7c1b
Pumacode04 <vivienht@hotmail.com>
2025-01-15 08:07:53 +0000

    Jan 15, 2025, 8:07 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -2,7 +2,7 @@ print("Hello from lesson 2")
 
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
-# 
+# set 
 
 
 ######## Write the pseudocode in comments for task 3 here
