ffec9471b1fda4846617550c473dfd5a1fe3deb9
Pumacode04 <vivienht@hotmail.com>
2025-01-15 08:41:20 +0000

    Jan 15, 2025, 8:41 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -10,14 +10,7 @@ print("Hello from lesson 2")
 # check each item's material and place it in the correct bin.
 
 # repeat until number of items is equal to 0
-#  check if item type is "glass", "plastic", or "paper"
-#   if item type is "glass"
-#       place it in the blue bin
-#   otherwise if its "plastic"
-#       place it in the green bin
-#   else
-#       place it in the yellow bin
-# decrease item by 1
+# 
 
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
