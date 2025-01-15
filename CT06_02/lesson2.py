89a93154b023e23e4c06cf7a1dc53186c5442971
Pumacode04 <vivienht@hotmail.com>
2025-01-15 08:41:43 +0000

    Jan 15, 2025, 8:41 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -3,13 +3,14 @@ print("Hello from lesson 2")
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
 
-# Task: Write down the steps on how to solve the problem below
-
-# Design pseudocode for a recycling robot that sorts items
-# into bins for glass, plastic, and paper. The robot should
-# check each item's material and place it in the correct bin.
-
-# 
 
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
+
+# Task: Using pseudocode, design a program that will check if the
+# user has entered the right secret phrase.
+
+# 1. If the user has entered a wrong secret phrase, send them an
+# error message.
+# 1. If the user has entered the right secret phrase, congratulate
+# them.
