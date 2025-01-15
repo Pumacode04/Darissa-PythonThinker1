c9b096927b3800d0d0825fd3387ccdf63bf15dd9
Pumacode04 <vivienht@hotmail.com>
2025-01-15 08:08:45 +0000

    Jan 15, 2025, 8:08 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -3,7 +3,8 @@ print("Hello from lesson 2")
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
 # set counter to 0
-# repeat until counter is equal 
+# repeat until counter is equal to 50
+#  
 
 
 ######## Write the pseudocode in comments for task 3 here
