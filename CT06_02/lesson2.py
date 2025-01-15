3e26c07c9a5b7934e5dd6215a090c3e52f965297
Pumacode04 <vivienht@hotmail.com>
2025-01-15 08:45:06 +0000

    Jan 15, 2025, 8:45 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -18,4 +18,5 @@ print("Hello from lesson 2")
 # set secrect phrase to : 20 september 2014
 # ask, "What is the secret phrase"
 # if reply is "20 september 2014"
-#   reply, "Congratulate"
+#   reply, "Congratulations!"
+# otherwise
