2b4586662ed820fd45bafd01888de5006e5e7af6
Pumacode04 <vivienht@hotmail.com>
2025-01-15 08:21:32 +0000

    Jan 15, 2025, 8:21 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -3,6 +3,11 @@ print("Hello from lesson 2")
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
 
+# Task: Write down the steps on how to solve the problem below
+
+# Design pseudocode for a recycling robot that sorts items
+# into bins for glass, plastic, and paper. The robot should
+# check each item's material and place it in the correct bin.
 
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
