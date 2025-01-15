b0e83565ab0e9867fcb03aa91a6d27f63b91fab4
Pumacode04 <vivienht@hotmail.com>
2025-01-15 08:13:14 +0000

    Jan 15, 2025, 8:13 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -2,12 +2,7 @@ print("Hello from lesson 2")
 
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
-# set counter to 0
-# repeat until counter is equal to 50
-#   move 10 steps
-#   turn clockwise 15 degrees
-#   change counter by 10
-
+# ask ""
 
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
