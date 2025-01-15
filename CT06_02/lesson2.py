a7a0cbcd84fdbd5f3f669c98e7ac1e015824f3fe
Pumacode04 <vivienht@hotmail.com>
2025-01-15 08:13:50 +0000

    Jan 15, 2025, 8:13 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -3,7 +3,7 @@ print("Hello from lesson 2")
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
 # ask "What is your age?"
-# if 
+# if answer is less than 18
 
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
