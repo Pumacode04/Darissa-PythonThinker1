28979f72b8654a040d9db42cd4b1a56bb883409b
Pumacode04 <vivienht@hotmail.com>
2025-01-15 09:02:04 +0000

    Jan 15, 2025, 9:02 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -40,4 +40,4 @@ print("Hello from lesson 2")
 # assign answer to variable "Test 3 score"
 # assign "Test 1 score" * 0.2 to "Total percentage"
 # assign "Test 2 score" * 0.4 to "total percentage"
-# assign "Test 3 score" *
+# assign "Test 3 score" * 0.4 to "T"
