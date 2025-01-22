1bdded89f6bef2de65f40761378c3190b283eca8
Pumacode04 <vivienht@hotmail.com>
2025-01-22 07:45:06 +0000

    Jan 22, 2025, 7:45 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -2,5 +2,16 @@
 
 answer = input("What is your name? ")
 print(answer)
+## Recap 1: Pseudo Code Practice
+# Write a pseudocode that calculates the average score of the 4
+# students:
 
-# Adam = 
+# Adam: 60
+# Brandon: 58
+# Charlie: 98
+# Daniel: 79
+
+
+
+# Adam = 60
+# Brandon = 
