9d9958fee84f40bd3655a0bb5cd2f9422fb22452
Pumacode04 <vivienht@hotmail.com>
2025-02-05 08:21:56 +0000

    Feb 5, 2025, 8:21 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -76,7 +76,7 @@ Name = input("Tell me your name.")
 print("Hi," + (Name))
 
 Hobby = input("Tell me your favourite hobby.")
-print("Hi, + (Hobby)")
+print("Hi," + (Hobby))
 
 DreamVacation = input("Tell me your dream vacation.")
 print("Hi, + (DreamVacation)")
