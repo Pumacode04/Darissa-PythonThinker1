50a1584e617b46ee1cdb6cd03e31c94063529e0e
Pumacode04 <vivienht@hotmail.com>
2025-02-12 07:52:32 +0000

    Feb 12, 2025, 7:52 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -20,4 +20,5 @@
 # Name = input("Tell me your name.")
 # Age = input("Age you are turning this year.")
 # Message = input("What do you want to say.")
-print("Happy " + Age + "th brithday " + Name + "! " + Message)
+# print("Happy " + Age + "th brithday " + Name + "! " + Message)
+
