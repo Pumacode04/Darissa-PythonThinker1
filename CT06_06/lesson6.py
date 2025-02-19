d3645d6d94289bd7e131dc186d38c6c189e5f70e
Pumacode04 <vivienht@hotmail.com>
2025-02-19 08:05:50 +0000

    Feb 19, 2025, 8:05 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -15,7 +15,7 @@
 Student = input("Number of students?")
 Total = 0
 for (i) in range(int(Student)):
-    Total = Student + input("Score of student?")
+    Total = Student + int(input("Score of student?"))
 Total = Total / Student
 print(Total)
 
