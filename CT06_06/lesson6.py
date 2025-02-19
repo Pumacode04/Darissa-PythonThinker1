2239c1b309746b142a9aa201500f05c03ab0206b
Pumacode04 <vivienht@hotmail.com>
2025-02-19 07:36:34 +0000

    Feb 19, 2025, 7:36 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,3 +1,3 @@
 Uhm = input("What is your name.")
-print("Hi" + Uhm + ". Please so kindly close this tab and log out of python.")
+print("Hi " + Uhm + ". Please so kindly close this tab and log out of python.")
 
