06d8dbc7a91ccc7e3d90eace98d35b6b5e78d2fa
Pumacode04 <vivienht@hotmail.com>
2025-02-26 07:45:01 +0000

    Feb 26, 2025, 7:45 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -5,7 +5,7 @@
 ### There is a total of 3 bugs in the following program.
 ### Identify and fix the bugs:
 
-score_one = 80
+# score_one = 80
 score_two = 90
 score_three = 75
 
@@ -13,6 +13,6 @@ total = score_one + score_two + score_three
 
 average_score = total / 3
 
-student_name = "Alex"
+# student_name = "Alex"
 
-print("Average score for " + str(student_name) + " is: " + str(average_score))
+# print("Average score for " + str(student_name) + " is: " + str(average_score))
