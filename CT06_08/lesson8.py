29fcf9dff5682bea2d17ba8f97f22bf3d811f76d
Pumacode04 <vivienht@hotmail.com>
2025-03-05 07:58:12 +0000

    Mar 5, 2025, 7:58 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,5 +1,5 @@
 #Task 1
 # # The step above is just asking for your name
-print("Nice to meet you, " + Name + "!")
+# print("Nice to meet you, " + Name + "!")
 # This other step above you is just saying "Nice to meet you," which is what it is supposed
-# (continue from previous comment) say and the name you typed in eariler.
+# #  (continue from previous comment) say and the name you typed in eariler.
