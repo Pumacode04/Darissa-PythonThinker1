e767f24de7c816d5f3667054c0e60808e52ed4a3
Pumacode04 <vivienht@hotmail.com>
2025-03-05 07:58:33 +0000

    Mar 5, 2025, 7:58 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,4 +1,4 @@
-#Task 1
+# Task 1
 # # The step above is just asking for your name
 # print("Nice to meet you, " + Name + "!")
 # This other step above you is just saying "Nice to meet you," which is what it is supposed
