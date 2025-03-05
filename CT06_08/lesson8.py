7b64acdb2439b2988df21904d07449955607568e
Pumacode04 <vivienht@hotmail.com>
2025-03-05 08:09:12 +0000

    Mar 5, 2025, 8:09 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -6,10 +6,10 @@
 # # (continue from previous comment) say and the name you typed in eariler.
 
 # Task 2
-Start = int(input("State your first number which will be your start."))
-Stop = int(input("State your second number which will be which number you stop at."))
-Increment = int(input("State your last number now which would be the difference of the previous number in the sequence."))
-# The top three codes are storing your choice of numbers in a variable
+# Start = int(input("State your first number which will be your start."))
+# Stop = int(input("State your second number which will be which number you stop at."))
+# Increment = int(input("State your last number now which would be the difference of the previous number in the sequence."))
+##  The top three codes are storing your choice of numbers in a variable
 for i in range(Start, Stop, Increment):
     print(i)
 # The progame is just printing what you requested for.
