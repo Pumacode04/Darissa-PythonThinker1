93df4e7510b1b9f2f9e0016618e877cb35faff8d
Pumacode04 <vivienht@hotmail.com>
2025-03-05 07:56:55 +0000

    Mar 5, 2025, 7:56 AM

--- CT06_08/lesson8.py
+++
@@ -1,5 +0,0 @@
-Name = input("What is your name? ")
-# The step above is just asking for your name
-print("Nice to meet you, " + Name + "!")
-# This other step above you is just saying "Nice to meet you," which is what it is supposed
-# (continue from previos comment) say and the name you typed in eariler.
