d258403d65a476ff08509b229eab63c237ccedaf
Pumacode04 <vivienht@hotmail.com>
2025-03-05 07:52:56 +0000

    Mar 5, 2025, 7:52 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,4 +1,4 @@
 Name = input("What is your name? ")
 # The step above is just asking for your name
 print("Nice to meet you, " + Name + "!")
-# This other step above you is just 
+# This other step above you is just saying "Nice to meet you,"
