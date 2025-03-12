37838323cbea77020c8c6fe553ee4dc1194f2f98
Pumacode04 <vivienht@hotmail.com>
2025-03-12 07:36:39 +0000

    Mar 12, 2025, 7:36 AM

---
+++ CT06_08/lesson8.py
@@ -0,0 +1,10 @@
+# Lesson 8 - Importing Libraries, Boolean & Conditions
+
+## Recap 1: Product of 5 numbers
+
+# Write a program to calculate the product (multiplication) of 5
+# numbers.
+
+# 1. Using a for loop, ask the user for 5 numbers one at a time.
+# 2. Calculate the multiplication for these 5 numbers and print
+#    it out.
