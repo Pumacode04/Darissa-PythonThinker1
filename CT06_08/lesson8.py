ee4548522352e509f8a77336b33611b1bef8bc06
Pumacode04 <vivienht@hotmail.com>
2025-03-12 07:44:45 +0000

    Mar 12, 2025, 7:44 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -8,9 +8,5 @@
 # 1. Using a for loop, ask the user for 5 numbers one at a time.
 # 2. Calculate the multiplication for these 5 numbers and print
 #    it out.
-first = input("What is number #1.")
-second = input("What is number #2.")
-third = input("What is number #3.")
-fourth = input("What is number #4.")
-fifth = input("What is number #5.")
-print(int(first) * int(second) * int(third) * int(fourth) * int(fifth))
+for i in range(1, 6) :
+    number = input("What is your #" + str(i))
