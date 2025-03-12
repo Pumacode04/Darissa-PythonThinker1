26de3909a2f1f695813e61135a0ecc654bbd30a9
Pumacode04 <vivienht@hotmail.com>
2025-03-12 07:56:23 +0000

    Mar 12, 2025, 7:56 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -25,3 +25,8 @@
 # Modify your code from Task 1a to include an 'input()' function
 # asking the user for the number to countdown from, before
 # counting down every second from the number given by the user.
+
+import time
+for i in range(10, 0, -1):
+    print(i)
+    time.sleep(1)
