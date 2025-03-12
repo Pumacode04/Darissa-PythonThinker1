55bf2f8fd7f2aac0d80f6d48b61efb3445da39a2
Pumacode04 <vivienht@hotmail.com>
2025-03-12 07:36:29 +0000

    Mar 12, 2025, 7:36 AM

---
+++ CT06_08/assessment.py
@@ -0,0 +1,15 @@
+# Task 1
+# Name = input("What is your name?")
+# # The step above is just asking for your name
+# print("Nice to meet you, " + Name + "!")
+# This other step above you is just saying "Nice to meet you," which is what it is supposed
+# # (continue from previous comment) say and the name you typed in eariler.
+
+# Task 2
+# Start = int(input("State your first number which will be your start."))
+# Stop = int(input("State your second number which will be which number you stop at."))
+# Increment = int(input("State your last number now which would be the difference of the previous number in the sequence."))
+# #   The top three codes are storing your choice of numbers in a variable
+#  i in range(Start, Stop, Increment):
+#     print(i)
+##  The progame is just printing what you requested for.
