15f36394a8541b0638b5f053d60b35de203c3558
Pumacode04 <vivienht@hotmail.com>
2025-03-26 08:21:25 +0000

    Mar 26, 2025, 8:21 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -8,8 +8,19 @@
 # 3. Use an 'else' statement for when the number is not greater than 0.
 #         In this case, print "{number} is negative."
 
-num = input("Choose a number.")
-if int(num) > 0:
-    print("Positive")
-else:
-    print("Negative")
+# num = input("Choose a number.")
+# if int(num) > 0:
+#     print("Positive")
+# else:
+#     print("Negative")
+
+    
+# Task 2: Random Number Guesser III
+# Create a Random Number Guesser program
+
+# 1. Generate a random integer between 1 to 10
+# 2. Ask the user to guess a number
+# 3. If the user guesses correctly:
+#     print "Congratulations!! You did it!"
+# 4. If the user guesses wrongly: 
+#     print "Oops, better luck next time!"
