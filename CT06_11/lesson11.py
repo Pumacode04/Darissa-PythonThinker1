188428f23330e3381010edf453d9a5d2d46ae97b
Pumacode04 <vivienht@hotmail.com>
2025-04-02 07:48:19 +0000

    Apr 2, 2025, 7:48 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -10,12 +10,12 @@
 #     px <= 500: "Where are you getting this money from?!"
 #     px > 500: "Don't even think about it!"
 
-px = input("Cost of your item?")
-if int(px) <= 5:
-    print("Sounds good!")
-elif int(px) <= 50:
-    print("Are you sure you need this?")
-elif int(px) <= 500:
-    print("Where are you getting this money from?!")
-else : 
-    print("Don't even think about it!")
+# px = input("Cost of your item?")
+# if int(px) <= 5:
+#     print("Sounds good!")
+# elif int(px) <= 50:
+#     print("Are you sure you need this?")
+# elif int(px) <= 500:
+#     print("Where are you getting this money from?!")
+# else : 
+#     print("Don't even think about it!")
