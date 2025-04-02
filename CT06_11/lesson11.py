3943bd898a976d876b5eb7a4e763e507f7e7b8eb
Pumacode04 <vivienht@hotmail.com>
2025-04-02 07:42:41 +0000

    Apr 2, 2025, 7:42 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -13,7 +13,7 @@
 px = input("Cost of your item?")
 if int(px) <= 5:
     print("Sounds good!")
-elif px <= 50:
+elif int(px) <= 50:
     print("Are you sure you need this?")
 elif px <= 500:
     print
