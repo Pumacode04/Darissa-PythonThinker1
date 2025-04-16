8c6a5864a7a71b0cc8bda2da073d5a4edb2da437
Pumacode04 <vivienht@hotmail.com>
2025-04-16 07:53:18 +0000

    Apr 16, 2025, 7:53 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,3 +1,13 @@
-print("Hello from lesson 13")
+# Task 1: List of groceries
+# **Task 1a**:
+# Create a list of 8 groceries you need to buy:
+# 1. Apples
+# 2. Bread
+# 3. Carrots
+# 4. Dates
+# 5. Eggs
+# 6. Flour
+# 7. Grapes
+# 8. Honey
 
-food = [ "Apples", "Bread", "Carrots", "Dates", ""]
+food = [ "Apples", "Bread", "Carrots", "Dates", "Eggs", ]
