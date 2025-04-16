624d7255f6995fa023cfb8a5a1ef5110b960ed97
Pumacode04 <vivienht@hotmail.com>
2025-04-16 08:47:23 +0000

    Apr 16, 2025, 8:47 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -78,6 +78,8 @@ wants = input("What are you looking for?")
 for items in store:
     if wants == items:
         print("Yes we sell that.")
+        break
     else:
         print("Sorry, we dont have that.")
+        
     
