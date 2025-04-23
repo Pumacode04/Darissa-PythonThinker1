239d8e2e813dd01b79cc30352dcdde2d7500c0a0
Pumacode04 <vivienht@hotmail.com>
2025-04-23 08:59:27 +0000

    Apr 23, 2025, 8:59 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -6,7 +6,7 @@ window.setup(width=600, height=400)
 t = turtle.Turtle()
 t.shape("turtle")
 t.fillcolor("orange")
-for i in range(4):
+for i in range(6):
     t.forward(50)
-    t.left(90)
+    t.left(60)
 window.mainloop()
