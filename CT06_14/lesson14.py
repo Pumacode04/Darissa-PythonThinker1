4a875a76579eafc0e312268d65a3f9dcd5e1dd9c
Pumacode04 <vivienht@hotmail.com>
2025-04-23 08:58:01 +0000

    Apr 23, 2025, 8:58 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -5,7 +5,7 @@ window = turtle.Screen()
 window.setup(width=600, height=400)
 t = turtle.Turtle()
 t.shape("turtle")
-t.fillcolor("red")
+t.fillcolor("orange")
 for i in range(4):
     t.forward(50)
     t.left(90)
