8ebd2975644725b334559c26c4b2d5aa781945cd
Pumacode04 <vivienht@hotmail.com>
2025-04-23 08:00:20 +0000

    Apr 23, 2025, 8:00 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -108,4 +108,4 @@ WinNum = []
 for i in range(10):
     LuckNum = random.randint(1, 9999)
     WinNum.append(LuckNum)
-    print("Winner #" + i + ": " + LuckNum[i])
+    print("Winner #" + str(i + ": " + LuckNum[i])
