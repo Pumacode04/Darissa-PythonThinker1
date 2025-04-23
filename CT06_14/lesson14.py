6bae8ddfbd111beee4c9abad589edf30a15c3a74
Pumacode04 <vivienht@hotmail.com>
2025-04-23 09:02:03 +0000

    Apr 23, 2025, 9:02 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -1,12 +1,12 @@
 # print("Hello from lesson 15")
 
-import turtle
-window = turtle.Screen()
-window.setup(width=600, height=400)
-t = turtle.Turtle()
-t.shape("turtle")
-t.fillcolor("orange")
-for i in range(6):
-    t.forward(50)
-    t.left(60)
-window.mainloop()
+# import turtle
+# window = turtle.Screen()
+# window.setup(width=600, height=400)
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.fillcolor("orange")
+# for i in range(6):
+#     t.forward(50)
+#     t.left(60)
+# window.mainloop()
