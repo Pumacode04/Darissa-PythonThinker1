4f28f037f1a18c3f7e86c7ad2e8b48f5386de7ba
Pumacode04 <vivienht@hotmail.com>
2025-04-30 08:27:28 +0000

    Apr 30, 2025, 8:27 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -21,7 +21,7 @@ for i in range(10):
     y = random.randint(-280, 280)
     t.goto(x, y - 40)
     for i in range(4):
-        t.foward(5)
+        t.forward(5)
         t.left(90)
     t.write(str(x) + ", " + str(y))
 window.mainloop()
