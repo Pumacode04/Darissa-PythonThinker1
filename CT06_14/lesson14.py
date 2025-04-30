98c73a20373edd28561683c5029d8edc16d29652
Pumacode04 <vivienht@hotmail.com>
2025-04-30 07:48:12 +0000

    Apr 30, 2025, 7:48 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -1,10 +1,11 @@
 # print("Hello from lesson 15")
 
 import turtle
-window = turtle.Screen
-t = turtle.Turtle
+window = turtle.Screen()
+t = turtle.Turtle()
 window.setup(width=600, height=400)
 counter = input("Number of sides?")
 for i in range(counter):
     t.forward(50)
     t.left(360 / counter)
+window.mainloop()
