b8b6165192c36093f7a7adcda15179c1e9635ddd
Pumacode04 <vivienht@hotmail.com>
2025-04-30 07:56:20 +0000

    Apr 30, 2025, 7:56 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -3,8 +3,8 @@
 # import turtle
 # window = turtle.Screen()
 # t = turtle.Turtle()
-window.setup(width=600, height=400)
-for i in range(5):
-    t.forward(50)
-    t.left(72)
+# window.setup(width=600, height=400)
+# for i in range(5):
+#     t.forward(50)
+#     t.left(72)
 window.mainloop()
