df6d9e9071fb4d71331234f1f63a8e7a5c38d67c
Pumacode04 <vivienht@hotmail.com>
2025-04-30 08:23:19 +0000

    Apr 30, 2025, 8:23 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -19,8 +19,7 @@ t.color("orange")
 for i in range(10):
     x = random.randint(-280, 280)
     y = random.randint(-280, 280)
-    t.goto(x, y)
-    t.
+    t.goto(x, y - 40)
     for i in range(4):
         t.foward(5)
         t.left(90)
