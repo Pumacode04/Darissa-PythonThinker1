5b3f10ebbf80b4f5d8cc485ab3fad8d5c85e963c
Pumacode04 <vivienht@hotmail.com>
2025-04-30 08:47:34 +0000

    Apr 30, 2025, 8:47 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -9,4 +9,24 @@
 #     t.left(72)
 # window.mainloop()
 
-#
+import random
+import turtle
+window = turtle.Screen()
+t = turtle.Turtle()
+window.setup(width=600, height=600)
+t.shape("turtle")
+t.color("orange")
+for i in range(10):
+    x = random.randint(-280, 280)
+    y = random.randint(-280, 280)
+    t.up()
+    t.goto(x, y)
+    t.down()
+    for i in range(4):
+        t.forward(5)
+        t.left(90)
+    t.up()
+    t.sety(y - 40)
+    t.down()
+    t.write(str(x) + ", " + str(y))
+window.mainloop()
