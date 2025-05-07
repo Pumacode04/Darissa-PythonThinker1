8d7cc8b951e32b9168faa1ce2dc2b14a9d9cce17
Pumacode04 <vivienht@hotmail.com>
2025-05-07 08:46:28 +0000

    May 7, 2025, 8:46 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -21,13 +21,13 @@
 
 #Task 1
 
-planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
+# planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
 # 1. Write code below to print the 3rd item 
 #    in this list using index e.g. earth
-print(planets[2])
+# print(planets[2])
 #The first item index is 0
 # 2. Write code to append neptune to this list.
-planets.append("neptune")
+# planets.append("neptune")
 # 3. Elon Musk has conquered Mars. 
 #    Rename Mars in the list to be "muskworld"
 planets[3] = "muskworld"
