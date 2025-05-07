cc4a96cac8ba9c0e31758d1e466a89582942e263
Pumacode04 <vivienht@hotmail.com>
2025-05-07 07:54:18 +0000

    May 7, 2025, 7:54 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -1,10 +1,10 @@
 # print("Exam says hi")
 
 #Task 1 :
-counter = 10
-while True :
-    if counter < 201:
-        print(counter)
-        counter = counter + 10
-    else:
-        break
+# counter = 10
+# while True :
+#     if counter < 201:
+#         print(counter)
+#         counter = counter + 10
+#     else:
+#         break
