5b502cef1e8c6eededc43913ed8c7bff88b48c3b
Pumacode04 <vivienht@hotmail.com>
2025-05-14 08:31:50 +0000

    May 14, 2025, 8:31 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -55,3 +55,12 @@
 # for num in nums:
 #     print(doubleNumber(num))
 
+
+## Task 6: Greetings III
+# Create a function that takes in a name and returns a greeting
+
+# 1. Create a function named 'greet()'
+# 2. The function should return a greeting
+#     Example: "Hello there <name>!"
+# 3. Ask the user for their name
+# 4. Using the 'greet()' function, print the greeting
