d45956bc1ea73d34ca083667fc1c348637ac63f3
Pumacode04 <vivienht@hotmail.com>
2025-05-14 08:10:26 +0000

    May 14, 2025, 8:10 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -16,3 +16,16 @@
 # 3. If the person is none of the above, say:
 #         "I don't think you belong here..."
 
+## Task 3: Increment Counter
+# Create a function that will increase the 'counter' variable by 1 each
+# time it is called.
+
+# 1. Create a 'counter' variable and assign it '0'
+# 2. Define a function 'increment_counter()':
+#         a. Declare 'counter' as 'global'
+#         b. Add 1 to 'counter'
+# 3. Test your program out by calling the 'increment_counter()' function
+# 4. 3 times before printing out the value of the 'counter' variable.
+
+# Your output should be "3"
+
